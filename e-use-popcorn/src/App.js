210c57f10b30b47e42ed8ceeb76e0db06ab5a867
brseghese <brseghese@gmail.com>
2023-12-24 06:02:58 -0300

    feat(use-popcorn): passing element as props (alternative)

--- e-use-popcorn/src/App.js
+++ e-use-popcorn/src/App.js
@@ -61,13 +61,23 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          }
+        />
+
+        {/* <Box>
           <MovieList movies={movies} />
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
-        </Box>
+        </Box> */}
       </Main>
     </>
   );
@@ -117,7 +127,7 @@ function Main({ children }) {
   return <main className='main'>{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -125,7 +135,7 @@ function Box({ children }) {
       <button className='btn-toggle' onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? '\u2212' : '\u002B'}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
